694dfa5056575f207308b0245fe01791c55cc7f6
Wendel Lopes <wendelopes@msn.com>
2021-07-09 08:50:07 -0300

    Adding controller to lives

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
+
   mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
+      resources :lives
+    end
+  end
 end
---
+++ lib/api_version_constraint.rb
@@ -0,0 +1,11 @@
+class ApiVersionConstraint
+
+  def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(req)
+    @default || req.headers['Accept'].include?("application/vnd.easelives.v#{@version}")
+  end
+end

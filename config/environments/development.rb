0fb79434e3163e2600436d1447f9d3b34571a43c
Wendel Lopes <wendelopes@msn.com>
2021-07-09 08:05:33 -0300

    Created authenticate user with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.hosts << "api.easy-lives.test"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -32,8 +33,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
